dec4a33122e6c512153c9cd2cc02964ccd6c2556
Thomasdev28 <boulanger.cap@hotmail.fr>
2021-03-18 09:38:05 +0100

    ajout de la structure contact

---
+++ contact.js
@@ -0,0 +1,4 @@
+const myButton = document.getElementById("onebtn");
+
+myButton.onclick = function () { prompt('Merci de votre attention les collegues wilders')};
+myButton.touchstart = function () { prompt('Merci de votre attention les collegues wilders')};
